d7f946b587dde76f5d3d75290e14609b8a3523b9
Janice <jadeng3@s.sfusd.edu>
2023-04-10 12:06:38 -0700

    completed 5.4 step 5

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -5,12 +5,14 @@ using TMPro;
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
+
 public class GameManager : MonoBehaviour
 {
     public List<GameObject> targets;
     public TextMeshProUGUI scoreText;
     public TextMeshProUGUI gameOverText;
     public Button restartButton;
+    public GameObject titleScreen;
     public bool isGameActive; 
     private int score;
     private float spawnRate = 1.0f;
@@ -18,10 +20,7 @@ public class GameManager : MonoBehaviour
      // Start is called before the first frame update
     void Start()
     {
-        isGameActive = true;
-        StartCoroutine(SpawnTarget());
-        score = 0;
-        UpdateScore(0);
+        
     }
 
     // Update is called once per frame
@@ -59,4 +58,16 @@ public class GameManager : MonoBehaviour
     {
         SceneManager.LoadScene(SceneManager.GetActiveScene().name);
     }
-}
+
+    public void StartGame()
+    {
+
+        isGameActive = true;
+        StartCoroutine(SpawnTarget());
+        score = 0;
+        UpdateScore(0);
+
+        titleScreen.gameObject.SetActive(false);
+    }
+
+    }
